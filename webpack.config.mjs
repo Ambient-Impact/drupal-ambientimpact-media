860daa697debfbca40b1355a6a5349a907eecfb8
Matei Stanca <i@ambientimpact.com>
2023-11-20 15:47:46 -0500

    webpack.config.mjs: CommonJS comment clarification + trailing commas.

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -3,19 +3,18 @@ import Encore from '@symfony/webpack-encore';
 import * as path from 'node:path';
 import { default as vendorize } from 'pnp-vendorize';
 
-// The remaining modules do not yet have ESM versions and so are CommonJS only.
-// Because of this, they must be import()ed and destructured like so to behave
-// similarly to ESM imports.
+// The remaining modules are CommonJS only. Because of this, they must be
+// import()ed and destructured like so to behave similarly to ESM imports.
 const { default: autoprefixer } = await import('autoprefixer');
 const { default: componentPaths } = await import(
-  'drupal-ambientimpact-core/componentPaths'
+  'drupal-ambientimpact-core/componentPaths',
 );
 const { default: glob } = await import('glob');
 const { default: RemoveEmptyScriptsPlugin } = await import(
-  'webpack-remove-empty-scripts'
+  'webpack-remove-empty-scripts',
 );
 const { default: SVGSpritemapPlugin } = await import(
-  'svg-spritemap-webpack-plugin'
+  'svg-spritemap-webpack-plugin',
 );
 
 const distPath = '.webpack-dist';
