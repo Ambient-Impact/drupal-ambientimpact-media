1c758f86e0bbc18b2d3b579439b0a182cb7dfda2
Matei Stanca <i@ambientimpact.com>
2024-10-03 15:22:41 -0400

    Element\MediaPlayOverlay: Convert to use PHP attribute from annotation.

--- src/Element/MediaPlayOverlay.php
+++ src/Element/MediaPlayOverlay.php
@@ -2,20 +2,24 @@
 
 namespace Drupal\ambientimpact_media\Element;
 
+use Drupal\Core\Render\Attribute\RenderElement;
 use Drupal\Core\Render\Element\RenderElementBase;
 
 /**
  * Provides a media play overlay render element.
- *
- * @RenderElement("media_play_overlay")
  */
+#[RenderElement('media_play_overlay')]
 class MediaPlayOverlay extends RenderElementBase {
+
   /**
    * {@inheritdoc}
    */
   public function getInfo() {
+
     return [
-      '#theme'    => 'media_play_overlay',
+      '#theme' => 'media_play_overlay',
     ];
+
   }
+
 }
